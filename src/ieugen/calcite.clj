fbe28ebb830a16a64fdb1602ffd11f34c497e271
Eugen Stan <eugen.stan@netdava.com>
2021-04-29 22:38:31 +0300

    Initial commit with calcite

---
+++ src/ieugen/calcite.clj
@@ -0,0 +1,7 @@
+(ns ieugen.calcite
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/ieugen/calcite_test.clj
@@ -0,0 +1,7 @@
+(ns ieugen.calcite-test
+  (:require [clojure.test :refer :all]
+            [ieugen.calcite :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
